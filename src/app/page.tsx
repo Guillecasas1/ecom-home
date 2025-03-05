c70cae8e44608e2aca6c1332219996864848054f
Guillermo <guillermofrasquet95@gmail.com>
2025-03-05 17:14:05 +0100

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { redirect } from "next/navigation";
 
-export default async function Home() {
+export const dynamic = "force-dynamic";
+
+export default async function Home () {
   redirect("/admin");
 }
